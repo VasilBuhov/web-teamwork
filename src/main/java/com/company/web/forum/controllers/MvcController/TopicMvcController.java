e626ed1e28a73da0e3d2efd4fa27b8c97e1da38c
ARCH <minus273dot15@gmail.com>
2023-08-05 09:04:22 +0300

    Forum statistics on post detailed view are now working

--- src/main/java/com/company/web/forum/controllers/MvcController/TopicMvcController.java
+++ src/main/java/com/company/web/forum/controllers/MvcController/TopicMvcController.java
@@ -1,48 +1,48 @@
 package com.company.web.forum.controllers.MvcController;
 
 import com.company.web.forum.exceptions.EntityNotFoundException;
-import com.company.web.forum.helpers.AuthenticationHelper;
 import com.company.web.forum.helpers.PostMapper;
 import com.company.web.forum.models.*;
 import com.company.web.forum.services.PostService;
 import com.company.web.forum.services.TopicService;
+import com.company.web.forum.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/topics")
 public class TopicMvcController {
-    private final TopicService service;
+    private final TopicService topicService;
     private final PostMapper postMapper;
     private final PostService postService;
+    private final UserService userService;
 
     @Autowired
-    public TopicMvcController(TopicService service, PostMapper postMapper, PostService postService) {
-        this.service = service;
+    public TopicMvcController(TopicService service, PostMapper postMapper, PostService postService, UserService userService) {
+        this.topicService = service;
         this.postMapper = postMapper;
         this.postService = postService;
+        this.userService = userService;
     }
 
     @GetMapping
     public String showAllTopics(Model model) {
-        model.addAttribute("topics", service.get(new FilterTopicOptions()));
+        model.addAttribute("topics", topicService.get(new FilterTopicOptions()));
         return "TopicsView";
     }
 
     @GetMapping("/{id}")
     public String showSingleTopic(@PathVariable int id, Model model) {
         try {
-            Topic topic = service.get(id);
+            FilterTopicOptions filterTopicOptions = new FilterTopicOptions();
+            Topic topic = topicService.get(id);
             model.addAttribute("topic", topic);
             model.addAttribute("posts", topic.getPosts());
             model.addAttribute("tags", topic.getTags());
+            model.addAttribute("allTopics", topicService.get(filterTopicOptions));
+            model.addAttribute("allUsers", userService.getAllUsers());
             return "post_details";
         } catch (EntityNotFoundException e) {
             model.addAttribute("error", e.getMessage());
