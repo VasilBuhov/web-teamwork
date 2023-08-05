b4b4b6d5980f86cfb887dde04f23fd7bae4d6ee7
ARCH <minus273dot15@gmail.com>
2023-08-05 18:03:54 +0300

    Attempted fix on search bar

--- src/main/java/com/company/web/forum/controllers/MvcController/SearchMvcController.java
+++ src/main/java/com/company/web/forum/controllers/MvcController/SearchMvcController.java
@@ -6,8 +6,8 @@ import com.company.web.forum.services.TopicService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -20,13 +20,13 @@ public class SearchMvcController {
     public SearchMvcController(TopicService topicService) {
         this.topicService = topicService;
     }
-    @GetMapping("/{title}")
-    public String searchByTopicName (@PathVariable String title, Model model) {
+    @GetMapping
+    public String searchByTopicName (@RequestParam("search") String title, Model model) {
+        System.out.println("String title:" + title);
         FilterTopicOptions filterTopicOptions = new FilterTopicOptions();
         Optional<String> byTitle = Optional.of(title);
         filterTopicOptions.setTitle(byTitle);
         model.addAttribute("topicsByName", topicService.get(filterTopicOptions));
-
         return "SearchBarView";
     }
 }
