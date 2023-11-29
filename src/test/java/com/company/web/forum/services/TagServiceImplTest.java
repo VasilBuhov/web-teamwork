6b103c5d4570faff9e3a809fd85e127e746b4af3
VasilBuhov <vasil.ivanov.buhov@gmail.com>
2023-11-29 15:16:48 +0200

    test for .yml file for ci testing

--- src/test/java/com/company/web/forum/services/TagServiceImplTest.java
+++ src/test/java/com/company/web/forum/services/TagServiceImplTest.java
@@ -1,92 +1,92 @@
-package com.company.web.forum.services;
-
-import com.company.web.forum.exceptions.AuthorizationException;
-import com.company.web.forum.models.Tag;
-import com.company.web.forum.models.User;
-import com.company.web.forum.repositories.TagRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import static com.company.web.forum.Auxiliary.*;
-@ExtendWith(MockitoExtension.class)
-class TagServiceImplTest {
-
-    @Mock
-    TagRepository mockRepository;
-
-    @InjectMocks
-    TagServiceImpl service;
-
-    @Test
-    void delete_Should_ThrowException_When_UserIsNotAdminOrCreator() {
-        // Arrange
-        Tag mockTag = createMockTag();
-
-        Mockito.when(mockRepository.get(Mockito.anyInt()))
-                .thenReturn(mockTag);
-
-        Tag mockTaggy = Mockito.mock(Tag.class);
-        User mockUser = createMockUser();
-
-        // Act, Assert
-        Assertions.assertThrows(
-                AuthorizationException.class,
-                () -> service.delete(1, mockUser));
-    }
-
-    @Test
-    void delete_Should_CallRepository_When_UserIsAdmin() {
-        // Arrange
-        User mockUserAdmin = createMockAdmin();
-        Tag mockTag = createMockTag();
-
-        Mockito.when(mockRepository.get(Mockito.anyInt()))
-                .thenReturn(mockTag);
-
-        // Act
-        service.delete(1, mockUserAdmin);
-
-        // Assert
-        Mockito.verify(mockRepository, Mockito.times(1))
-                .delete(1);
-    }
-    @Test
-    void delete_Should_CallRepository_When_UserIsCreator() {
-        // Arrange
-        Tag mockTag = createMockTag();
-        User mockUserCreator = mockTag.getBelongs_to();
-
-        Mockito.when(mockRepository.get(Mockito.anyInt()))
-                .thenReturn(mockTag);
-
-        // Act
-        service.delete(1, mockUserCreator);
-
-        // Assert
-        Mockito.verify(mockRepository, Mockito.times(1))
-                .delete(1);
-    }
-
-
-    @Test
-    public void get_Should_ReturnBeer_When_MatchByIdExist() {
-        // Arrange
-        Tag mockTag = createMockTag();
-
-        Mockito.when(mockRepository.get(Mockito.anyInt()))
-                .thenReturn(mockTag);
-
-        // Act
-        Tag result = service.getTagById(mockTag.getId());
-
-        // Assert
-        Assertions.assertEquals(mockTag, result);
-    }
-
-
-}
+//package com.company.web.forum.services;
+//
+//import com.company.web.forum.exceptions.AuthorizationException;
+//import com.company.web.forum.models.Tag;
+//import com.company.web.forum.models.User;
+//import com.company.web.forum.repositories.TagRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import static com.company.web.forum.Auxiliary.*;
+//@ExtendWith(MockitoExtension.class)
+//class TagServiceImplTest {
+//
+//    @Mock
+//    TagRepository mockRepository;
+//
+//    @InjectMocks
+//    TagServiceImpl service;
+//
+//    @Test
+//    void delete_Should_ThrowException_When_UserIsNotAdminOrCreator() {
+//        // Arrange
+//        Tag mockTag = createMockTag();
+//
+//        Mockito.when(mockRepository.get(Mockito.anyInt()))
+//                .thenReturn(mockTag);
+//
+//        Tag mockTaggy = Mockito.mock(Tag.class);
+//        User mockUser = createMockUser();
+//
+//        // Act, Assert
+//        Assertions.assertThrows(
+//                AuthorizationException.class,
+//                () -> service.delete(1, mockUser));
+//    }
+//
+//    @Test
+//    void delete_Should_CallRepository_When_UserIsAdmin() {
+//        // Arrange
+//        User mockUserAdmin = createMockAdmin();
+//        Tag mockTag = createMockTag();
+//
+//        Mockito.when(mockRepository.get(Mockito.anyInt()))
+//                .thenReturn(mockTag);
+//
+//        // Act
+//        service.delete(1, mockUserAdmin);
+//
+//        // Assert
+//        Mockito.verify(mockRepository, Mockito.times(1))
+//                .delete(1);
+//    }
+//    @Test
+//    void delete_Should_CallRepository_When_UserIsCreator() {
+//        // Arrange
+//        Tag mockTag = createMockTag();
+//        User mockUserCreator = mockTag.getBelongs_to();
+//
+//        Mockito.when(mockRepository.get(Mockito.anyInt()))
+//                .thenReturn(mockTag);
+//
+//        // Act
+//        service.delete(1, mockUserCreator);
+//
+//        // Assert
+//        Mockito.verify(mockRepository, Mockito.times(1))
+//                .delete(1);
+//    }
+//
+//
+//    @Test
+//    public void get_Should_ReturnBeer_When_MatchByIdExist() {
+//        // Arrange
+//        Tag mockTag = createMockTag();
+//
+//        Mockito.when(mockRepository.get(Mockito.anyInt()))
+//                .thenReturn(mockTag);
+//
+//        // Act
+//        Tag result = service.getTagById(mockTag.getId());
+//
+//        // Assert
+//        Assertions.assertEquals(mockTag, result);
+//    }
+//
+//
+//}
--- src/test/java/com/company/web/forum/services/UserServiceImplTest.java
+++ src/test/java/com/company/web/forum/services/UserServiceImplTest.java
@@ -1,7 +1,61 @@
 package com.company.web.forum.services;
 
+import com.company.web.forum.exceptions.AuthorizationException;
+import com.company.web.forum.exceptions.EntityDuplicateException;
+import com.company.web.forum.exceptions.EntityNotFoundException;
+import com.company.web.forum.models.User;
+import com.company.web.forum.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Test
+    void getAllUsers_ShouldReturnAllUsers() {
+        // Arrange
+        when(userRepository.getAllUsers()).thenReturn(Collections.singletonList(new User()));
+
+        // Act
+        List<User> users = userService.getAllUsers();
+
+        // Assert
+        assertEquals(1, users.size());
+        verify(userRepository, times(1)).getAllUsers();
+    }
+
+
+
+    @Test
+    void createUser_ShouldThrowEntityDuplicateException_WhenDuplicateEmail() {
+        // Arrange
+        User user = new User();
+        user.setEmail("existing@example.com");
+        when(userRepository.getUserByEmail(anyString())).thenReturn(new User());
+
+        // Act, Assert
+        assertThrows(EntityDuplicateException.class, () -> userService.createUser(user));
+    }
+
+    // Add more test cases for other methods in UserServiceImpl
+    // ...
 
 }
